c7f7381a5f033df0307915312fcea5d542b86d4c
Lavakids <meinschatz27@hotmail.com>
2026-08-16 20:10:22 +0200

    Enhance service worker with better caching and fetch logic
    Refactor service worker to improve cache management and fetch handling.

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE="lavakids-v6";
-const ASSETS=["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png", "./home.png", "./food.png", "./clients.png", "./free_detail.png", "./history.png", "./alerts.png", "./birthdays.png", "./free.png", "./splash.png", "./loyalty.png", "./pack.png", "./detail.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method === "GET") {
+    event.respondWith(fetch(event.request, { cache: "no-store" }));
+  }
+});
+
